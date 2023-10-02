ea9ffbde88fd9434f8ce2d1d7c7f19987d102ecd
Noemiektz <146719685+Noemiektz@users.noreply.github.com>
2023-10-02 12:15:38 +0200

    Update exercise4.java

--- exercise4.java
+++ exercise4.java
@@ -1,1 +1,17 @@
+public class vars {
 
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        Integer i = 0;
+
+        while (i < str.length()) {
+
+            System.out.print(str.charAt(i));
+            i++;
+
+        }
+        System.out.print("\n");
+    }
+}
