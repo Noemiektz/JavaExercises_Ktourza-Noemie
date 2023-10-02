367abd7ffbd328a94a9785a652f12b056222694e
Noemiektz <146719685+Noemiektz@users.noreply.github.com>
2023-10-02 13:28:53 +0200

    Update exercice7.java

--- exercice7.java
+++ exercice7.java
@@ -1,1 +1,13 @@
-
+public class vars {
+    public static void main(String[] args) {
+       
+        
+        for(Integer i = 0;
+         i < 11;
+          i++){
+            System.out.println(i + "x 5 =" + i*5);
+        
+        
+        }
+    }
+}
