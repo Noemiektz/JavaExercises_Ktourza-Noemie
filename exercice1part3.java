6dba574bbcbf872e036b4a27856a5d9d0bc52c24
Noemiektz <146719685+Noemiektz@users.noreply.github.com>
2023-10-03 10:08:10 +0200

    Update exercice1part3.java

--- exercice1part3.java
+++ exercice1part3.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
+
+public class vars {
+    public static void main(String[] args) {
+       for(Integer i = 1; i < 101; i++;){
+           
+           if (i %3 == 0){
+            System.out.print("Fizz");
+        } else if (i %5 == 0){
+    System.out.print("Buzz");
+        }else if (i %5 == 0 && i %3 == 0){
+    System.out.print("FizzBuzz");
+        } else{
+            System.out.print(i);
+        }
+       
+       System.out.print(" ");
+       
+       }
+    }
+
+
+
+}
+
 
